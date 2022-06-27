444bc575629c2f76cbf7a5c8a633ddba062bd381
Aleksandr Panchenko <nemefis@yandex.ru>
2022-06-28 00:10:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// console.log("Hello world!");
+
+// let number = 5;
+// const leftBoerderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name: "Alex",
+//     age: 28,
+//     isMarried: false
+// };
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(typeof(answer), answer + 5);
+
+// const asnwers = [];
+
+// asnwers[0] = prompt('Ваше имя', '');
+// asnwers[1] = prompt('Ваша фамилия', '');
+// asnwers[2] = prompt('Ваш возраст', '');
+
+// console.log(typeof(asnwers));
+
+// const category = 'toys';
+
+// console.log(`https://test.ru/${category}`);
+
+// const user = 'Ivan';
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log('arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 4 * 2 === 8);
+
+const isChecked = true,
+        isClose = false;
+
+console.log(isChecked || !isClose);
