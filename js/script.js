f6897d3c84cf7dbf678be3ad09ab163b6bbb6fd7
Aleksandr Panchenko <nemefis@yandex.ru>
2022-06-28 22:18:38 +0300

    my first program

--- js/script.js
+++ js/script.js
@@ -1,57 +1,21 @@
 "use strict";
 
-// console.log("Hello world!");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// let number = 5;
-// const leftBoerderWidth = 1;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// number = 10;
-// console.log(number);
+const   a = prompt('Один из последних просмотренных фильмов', ''),
+        b = prompt('На сколько оцените его?', ''),
+        c = prompt('Один из последних просмотренных фильмов', ''),
+        d = prompt('На сколько оцените его?', '');
 
-// const obj = {
-//     name: "Alex",
-//     age: 28,
-//     isMarried: false
-// };
-// console.log(obj.name);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-
-// const answer = +prompt("Вам есть 18?", "");
-// console.log(typeof(answer), answer + 5);
-
-// const asnwers = [];
-
-// asnwers[0] = prompt('Ваше имя', '');
-// asnwers[1] = prompt('Ваша фамилия', '');
-// asnwers[2] = prompt('Ваш возраст', '');
-
-// console.log(typeof(asnwers));
-
-// const category = 'toys';
-
-// console.log(`https://test.ru/${category}`);
-
-// const user = 'Ivan';
-// alert(`Привет, ${user}`);
-
-console.log('arr' + " - object");
-console.log('arr' + " - object");
-
-let incr = 10,
-    decr = 10;
-
-// incr++;
-// decr--;
-
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-console.log(2 + 4 * 2 === 8);
-
-const isChecked = true,
-        isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(personalMovieDB);
